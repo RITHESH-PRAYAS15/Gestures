c80b94bf863e0a35718878447933d22012caa1e2
RITHESH-PRAYAS15 <rithesh-prayas15@users.noreply.github.com>
2026-03-12 18:25:40 +0530

    fix: removed hardcoded css to fix manifest load error

--- src/content/index.tsx
+++ src/content/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { CameraManager } from './CameraManager';
+import './index.css';
 
 // Inject the root element into the comic web page
 const injectUI = () => {
